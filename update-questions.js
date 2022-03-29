af0dd50fbaaf4903b55f269575223f92a231e639
Kevin Jerome <kjerome1997@gmail.com>
2022-03-28 22:19:53 -0700

    Add code to fetch questions from multiple categories

--- update-questions.js
+++ update-questions.js
@@ -4,10 +4,118 @@ const _ = require('lodash');
 const he = require('he');
 const axios = require('axios');
 
-async function fetchNewTriviaQuestions() {
+const categories = [
+  {
+    id: 9,
+    name: 'General Knowledge',
+  },
+  {
+    id: 10,
+    name: 'Entertainment: Books',
+  },
+  {
+    id: 11,
+    name: 'Entertainment: Film',
+  },
+  {
+    id: 12,
+    name: 'Entertainment: Music',
+  },
+  {
+    id: 13,
+    name: 'Entertainment: Musicals & Theatres',
+  },
+  {
+    id: 14,
+    name: 'Entertainment: Television',
+  },
+  {
+    id: 15,
+    name: 'Entertainment: Video Games',
+  },
+  {
+    id: 16,
+    name: 'Entertainment: Board Games',
+  },
+  {
+    id: 17,
+    name: 'Science & Nature',
+  },
+  {
+    id: 18,
+    name: 'Science: Computers',
+  },
+  {
+    id: 19,
+    name: 'Science: Mathematics',
+  },
+  {
+    id: 20,
+    name: 'Mythology',
+  },
+  {
+    id: 21,
+    name: 'Sports',
+  },
+  {
+    id: 22,
+    name: 'Geography',
+  },
+  {
+    id: 23,
+    name: 'History',
+  },
+  {
+    id: 24,
+    name: 'Politics',
+  },
+  {
+    id: 25,
+    name: 'Art',
+  },
+  {
+    id: 26,
+    name: 'Celebrities',
+  },
+  {
+    id: 27,
+    name: 'Animals',
+  },
+  {
+    id: 28,
+    name: 'Vehicles',
+  },
+  {
+    id: 29,
+    name: 'Entertainment: Comics',
+  },
+  {
+    id: 30,
+    name: 'Science: Gadgets',
+  },
+  {
+    id: 31,
+    name: 'Entertainment: Japanese Anime & Manga',
+  },
+  {
+    id: 32,
+    name: 'Entertainment: Cartoon & Animations',
+  },
+];
+
+function getCategoryIDsFromNames(categoryNames) {
+  const categoryIDs = categories
+    .filter((category) => categoryNames.includes(category.name.toLowerCase()))
+    .map((category) => category.id);
+  return categoryIDs;
+}
+
+async function fetchNewTriviaQuestions(amount, categoryID) {
   const {
     data: {results},
-  } = await axios.get('https://opentdb.com/api.php?amount=50');
+  } = await axios.get(
+    `https://opentdb.com/api.php?amount=${amount}&category=${categoryID}`
+  );
   return results;
 }
 
@@ -26,7 +134,22 @@ function sanitizeData(questions) {
 
 async function main() {
   try {
-    const questions = await fetchNewTriviaQuestions();
+    const categoryIDs = getCategoryIDsFromNames([
+      'history',
+      'geography',
+      'mythology',
+    ]);
+    console.log('Detected category IDs:', categoryIDs);
+
+    const questions = (
+      await Promise.all(
+        categoryIDs.map(
+          async (categoryID) => await fetchNewTriviaQuestions(50, categoryID)
+        )
+      )
+    ).flat();
+    console.log('Fetched ', questions.length, ' questions from Trivia API.');
+
     const sanitizedData = sanitizeData(questions);
     fs.writeFileSync(
       path.join(__dirname, 'data', 'questions.json'),
